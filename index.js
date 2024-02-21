3e024640608cb4504471a7bdf6fe57ab8bbaae69
Kristen <kgrace61@gmail.com>
2024-02-21 16:21:11 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31
+const num2 = 2
+const num3 = 5
+const num4 = 1 
+var multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) +1;
+
+let mod = num3 - num4 
+
+let max = Math.max(2, 4, 6, 20)
